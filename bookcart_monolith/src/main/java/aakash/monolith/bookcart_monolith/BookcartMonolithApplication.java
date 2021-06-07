a3120dc1311c579ed99757bd89e4ff9a81e37ae5
Aakash Mishra <aakashmishra1991@gmail.com>
2021-06-07 22:24:06 +0530

    First Commit = Monolith book cart Structure
    Monolith book cart Structure

---
+++ bookcart_monolith/src/main/java/aakash/monolith/bookcart_monolith/BookcartMonolithApplication.java
@@ -0,0 +1,13 @@
+package aakash.monolith.bookcart_monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcartMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcartMonolithApplication.class, args);
+	}
+
+}
---
+++ bookcart_monolith/src/test/java/aakash/monolith/bookcart_monolith/BookcartMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package aakash.monolith.bookcart_monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookcartMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
