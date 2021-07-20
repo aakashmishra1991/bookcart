67955b9b7cadf0a7d2ff874ec328ec8902a39910
abc <abc@example.com>
2021-07-20 21:05:50 +0530

    added all MSA projects
    added all projects

---
+++ microservices1/user/src/test/java/com/bookcart/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookcart.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
